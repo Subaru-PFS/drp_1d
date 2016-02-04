f8d9d78ce5d3618ce6a35b6304fccd7c9309ae52
Renato Callado Borges <rborges@hush.com>
2016-02-04 16:54:54 +0100

    - Added testMissingParameterOnJSONCausesDefault to lm2 func test suite - Added testInvalidParameterValueOnJSONCausesDefault to lm2 func test suite

--- test/functional/linematching2.py
+++ test/functional/linematching2.py
@@ -57,14 +57,14 @@ def wrapCall ( testFunctionName ):
         errorStrings.append ( "{}: {}".format ( testFunctionName.__name__, e ) )
     cleanup ( )
         
-def testGoodSpectrumYieldsGoodRedshift ( ):
+def testGoodSpectrumDynamicallyYieldsGoodRedshift ( ):
     spectrumlist = cpfPath + "/test/data/linematchingFunctional/batch6testGood.spectrumlist"
     amazed ( amazedExecutable,
              spectrumlist,
              cpfPath + "/test/data/linematchingFunctional/ExtendedGalaxyEL3",
              cpfPath + "/test/data/linematchingFunctional/batch6",
              cpfPath + "/test/data/linematchingFunctional/linecatalogamazedvacuum_B7C.txt",
-             cpfPath + "/test/data/linematchingFunctional/all_methods_dev.json" )
+             cpfPath + "/test/data/linematchingFunctional/linematching2Dynamic.json" )
     redshiftFile = open ( "output/redshift.csv" )
     redshiftString = redshiftFile.read ( )
     redshift = float ( redshiftString.split ( ) [ 1 ] )
@@ -75,14 +75,49 @@ def testGoodSpectrumYieldsGoodRedshift ( ):
         failureStrings.append ( "{}".format ( sys._getframe ( ).f_code.co_name ) )
         print ( "{}: redshift {} too different from 0.077164".format ( sys._getframe ( ).f_code.co_name, redshift ) )
 
-def testBadSpectrumYieldsBadRedshift ( ):
+def testGoodSpectrumStaticallyYieldsGoodRedshift ( ):
+    spectrumlist = cpfPath + "/test/data/linematchingFunctional/batch6testGood.spectrumlist"
+    amazed ( amazedExecutable,
+             spectrumlist,
+             cpfPath + "/test/data/linematchingFunctional/ExtendedGalaxyEL3",
+             cpfPath + "/test/data/linematchingFunctional/batch6",
+             cpfPath + "/test/data/linematchingFunctional/linecatalogamazedvacuum_B7C.txt",
+             cpfPath + "/test/data/linematchingFunctional/linematching2Static.json" )
+    redshiftFile = open ( "output/redshift.csv" )
+    redshiftString = redshiftFile.read ( )
+    redshift = float ( redshiftString.split ( ) [ 1 ] )
+    expectedRedshift = 0.077164
+    if expectedRedshift - 1e-4 <= redshift <= expectedRedshift + 1e-4:
+        successStrings.append ( "{}".format ( sys._getframe ( ).f_code.co_name ) )
+    else:
+        failureStrings.append ( "{}".format ( sys._getframe ( ).f_code.co_name ) )
+        print ( "{}: redshift {} too different from 0.077164".format ( sys._getframe ( ).f_code.co_name, redshift ) )
+
+def testBadSpectrumStaticallyYieldsBadRedshift ( ):
     spectrumlist = cpfPath + "/test/data/linematchingFunctional/batch6testBad.spectrumlist"
     amazed ( amazedExecutable,
              spectrumlist,
              cpfPath + "/test/data/linematchingFunctional/ExtendedGalaxyEL3",
              cpfPath + "/test/data/linematchingFunctional/batch6",
              cpfPath + "/test/data/linematchingFunctional/linecatalogamazedvacuum_B7C.txt",
-             cpfPath + "/test/data/linematchingFunctional/all_methods_dev.json" )
+             cpfPath + "/test/data/linematchingFunctional/linematching2Static.json" )
+    redshiftFile = open ( "output/redshift.csv" )
+    redshiftString = redshiftFile.read ( )
+    redshift = float ( redshiftString.split ( ) [ 1 ] )
+    expectedRedshift = 3.59831
+    if expectedRedshift - 1e-4 <= redshift <= expectedRedshift + 1e-4:
+        failureStrings.append ( "{}".format ( sys._getframe ( ).f_code.co_name ) )
+    else:
+        successStrings.append ( "{}".format ( sys._getframe ( ).f_code.co_name ) )
+
+def testBadSpectrumDynamicallyYieldsBadRedshift ( ):
+    spectrumlist = cpfPath + "/test/data/linematchingFunctional/batch6testBad.spectrumlist"
+    amazed ( amazedExecutable,
+             spectrumlist,
+             cpfPath + "/test/data/linematchingFunctional/ExtendedGalaxyEL3",
+             cpfPath + "/test/data/linematchingFunctional/batch6",
+             cpfPath + "/test/data/linematchingFunctional/linecatalogamazedvacuum_B7C.txt",
+             cpfPath + "/test/data/linematchingFunctional/linematching2Dynamic.json" )
     redshiftFile = open ( "output/redshift.csv" )
     redshiftString = redshiftFile.read ( )
     redshift = float ( redshiftString.split ( ) [ 1 ] )
@@ -99,7 +134,7 @@ def testInvalidFluxFilenameOnSpectrumlistCausesAbortion ( ):
                       cpfPath + "/test/data/linematchingFunctional/ExtendedGalaxyEL3",
                       cpfPath + "/test/data/linematchingFunctional/batch6",
                       cpfPath + "/test/data/linematchingFunctional/linecatalogamazedvacuum_B7C.txt",
-                      cpfPath + "/test/data/linematchingFunctional/all_methods_dev.json" )
+                      cpfPath + "/test/data/linematchingFunctional/linematching2Static.json" )
     logFile = open ( "output/log.txt" )
     logString = logFile.read ( )
     failed = False
@@ -138,12 +173,67 @@ def testInvalidParameterNameOnJSONCausesAbortion ( ):
     else:
         successStrings.append ( "{}".format ( sys._getframe ( ).f_code.co_name ) )
 
+def testMissingParameterOnJSONCausesDefault ( ):
+    spectrumlist = cpfPath + "/test/data/linematchingFunctional/batch6testGood.spectrumlist"
+    retval = amazed ( amazedExecutable,
+                      spectrumlist,
+                      cpfPath + "/test/data/linematchingFunctional/ExtendedGalaxyEL3",
+                      cpfPath + "/test/data/linematchingFunctional/batch6",
+                      cpfPath + "/test/data/linematchingFunctional/linecatalogamazedvacuum_B7C.txt",
+                      cpfPath + "/test/data/linematchingFunctional/linematching2MissingParameter.json" )
+    logFile = open ( "output/parameters.json" )
+    logString = logFile.read ( )
+    failed = False
+    if retval != 0:
+        failed = True
+        print ( "{}: Amazed failed to reduce good data, when parameter missing on JSON.".format ( sys._getframe ( ).f_code.co_name ) )
+    if not "\"minsize\": \"3\"" in logString:
+        failed = True
+        print ( "{}: Amazed did use default parameter value when parameter was missing in JSON.".format ( sys._getframe ( ).f_code.co_name ) )
+
+    if failed:
+        failureStrings.append ( "{}".format ( sys._getframe ( ).f_code.co_name ) )
+    else:
+        successStrings.append ( "{}".format ( sys._getframe ( ).f_code.co_name ) )
+
+def testInvalidParameterValueOnJSONCausesDefault ( ):
+    spectrumlist = cpfPath + "/test/data/linematchingFunctional/batch6testGood.spectrumlist"
+    retval = amazed ( amazedExecutable,
+                      spectrumlist,
+                      cpfPath + "/test/data/linematchingFunctional/ExtendedGalaxyEL3",
+                      cpfPath + "/test/data/linematchingFunctional/batch6",
+                      cpfPath + "/test/data/linematchingFunctional/linecatalogamazedvacuum_B7C.txt",
+                      cpfPath + "/test/data/linematchingFunctional/linematching2InvalidValue.json" )
+    failed = False
+    if retval != 0:
+        failed = True
+        print ( "{}: Amazed failed to reduce good data, when parameter has invalid value on JSON.".format ( sys._getframe ( ).f_code.co_name ) )
+    try:
+        logFile = open ( "output/parameters.json" )
+        logString = logFile.read ( )
+    except:
+        failed = True
+        print ( "{}: missing expected output/parameters.json file.".format ( sys._getframe ( ).f_code.co_name ) )
+        logString = ""
+    if "\"winsize\": \"-250\"" in logString:
+        failed = True
+        print ( "{}: Amazed did use invalid parameter value present in JSON.".format ( sys._getframe ( ).f_code.co_name ) )
+
+    if failed:
+        failureStrings.append ( "{}".format ( sys._getframe ( ).f_code.co_name ) )
+    else:
+        successStrings.append ( "{}".format ( sys._getframe ( ).f_code.co_name ) )
+        
 if __name__ == "__main__":
     cleanup ( )
-    wrapCall ( testGoodSpectrumYieldsGoodRedshift )
-    wrapCall ( testBadSpectrumYieldsBadRedshift )
+    wrapCall ( testGoodSpectrumStaticallyYieldsGoodRedshift )
+    wrapCall ( testGoodSpectrumDynamicallyYieldsGoodRedshift )
+    wrapCall ( testBadSpectrumStaticallyYieldsBadRedshift )
+    wrapCall ( testBadSpectrumDynamicallyYieldsBadRedshift )
     wrapCall ( testInvalidFluxFilenameOnSpectrumlistCausesAbortion )
     wrapCall ( testInvalidParameterNameOnJSONCausesAbortion )
+    wrapCall ( testMissingParameterOnJSONCausesDefault )
+    wrapCall ( testInvalidParameterValueOnJSONCausesDefault )
     
     printSuccesses ( )
     printFailures ( )
