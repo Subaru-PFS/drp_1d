2a5bc0c49a573b4857f7b31970c4cf6db030e53a
Alain Schmitt <alain.schmitt@lam.fr>
2017-01-17 11:37:53 +0100

    unit test lm-tplshape-catalog debug

--- test/linemodel_tplshapecatalog.cpp
+++ test/linemodel_tplshapecatalog.cpp
@@ -11,7 +11,7 @@ BOOST_AUTO_TEST_SUITE(LinemodelTplshapecatalog)
 
 BOOST_AUTO_TEST_CASE( LoadCatalogAndVelocities )
 {
-    std::string calibrationPath = "/home/aschmitt/gitlab/cpf-redshift/test/data/LinemodelTplshapeCtlgTestCase/calibrationPath1";
+    std::string calibrationPath = "../test/data/LinemodelTplshapeCtlgTestCase/calibrationPath1";
     std::string relPath = "linecatalogs_tplshape_ExtendedTemplatesMarch2016_B13B_mod20170110_3";
     CRayCatalogsTplShape* catalogTplShape = new CRayCatalogsTplShape();
     catalogTplShape->SetTplctlgRelPath(relPath.c_str());
