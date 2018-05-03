6f01ab173baf41b9fa0408375359122e3474b2c9
guillaume pernot <guillaume.pernot@lam.fr>
2018-05-03 15:23:50 +0200

    added config file parameter to simplify amazed.py command line

--- python/amazed.py
+++ python/amazed.py
@@ -2,38 +2,41 @@ import os.path
 
 from redshift import *
 from argumentparser import parser
+from config import Config
+
+def fullpath(config, *path):
+    return os.path.expanduser(os.path.join(config.data_path, *path))
 
 class TestReader(CSpectrumIOGenericReader):
 
     def Read(self, path, spectrum):
-        print('there')
         print('reading {} into {}'.format(path,spectrum))
         res = super().Read(path, spectrum)
         print('here')
         return res
 
-
 def amazed():
 
     args = parser.parse_args()
-    print(args)
+    config = Config(args)
+
     zlog = CLog()
     logConsoleHandler = CLogConsoleHandler( zlog )
     logConsoleHandler.SetLevelMask ( zlog.nLevel_Info )
 
     param = CParameterStore()
-    param.Load(os.path.join(args.data_path, args.parameters_file))
+    param.Load(fullpath(config, config.parameters_file))
 
     classif = CClassifierStore()
 
-    if args.zclassifier_dir:
-        classif.Load(args.zclassifier_dir)
+    if config.zclassifier_dir:
+        classif.Load(fullpath(config, config.zclassifier_dir))
 
     spectrumList = []
-    with open(os.path.join(args.data_path, args.input_file), 'r') as f:
+    with open(fullpath(config, config.input_file), 'r') as f:
         for spectrum in f:
             if not spectrum.startswith('#'):
-                spectrumList.append(spectrum.strip())
+                spectrumList.append(fullpath(config, config.spectrum_dir, spectrum.strip()))
 
     retcode, medianRemovalMethod = param.Get_String("continuumRemoval.method",
                                                     "IrregularSamplingMedian")
@@ -51,12 +54,12 @@ def amazed():
 
     template_catalog = CTemplateCatalog(medianRemovalMethod, opt_medianKernelWidth,
                                         opt_nscales, dfBinPath)
-    print("Loading %s" % args.template_dir)
-    template_catalog.Load(args.template_dir)
+    print("Loading %s" % config.template_dir)
+    template_catalog.Load(fullpath(config, config.template_dir))
 
     line_catalog = CRayCatalog()
-    print("Loading %s" % args.linecatalog)
-    line_catalog.Load(args.linecatalog)
+    print("Loading %s" % config.linecatalog)
+    line_catalog.Load(fullpath(config, config.linecatalog))
 
     reader = TestReader()
 
@@ -65,8 +68,8 @@ def amazed():
     for line in spectrumList:
         spectrum, noise, proc_id = line.split()
         ctx=CProcessFlowContext()
-        ctx.Init(os.path.join(args.spectrum_dir, spectrum),
-                 os.path.join(args.spectrum_dir, noise),
+        ctx.Init(fullpath(config, config.spectrum_dir, spectrum),
+                 fullpath(config, config.spectrum_dir, noise),
                  reader,
                  reader,
                  proc_id,
@@ -77,9 +80,9 @@ def amazed():
 
         pflow=CProcessFlow()
         pflow.Process(ctx)
-        ctx.GetDataStore().SaveRedshiftResult(args.output_folder)
+        ctx.GetDataStore().SaveRedshiftResult(config.output_folder)
         #ctx.GetDataStore().SaveReliabilityResult('/tmp/bar')
-        ctx.GetDataStore().SaveAllResults(args.output_folder, 'all')
+        ctx.GetDataStore().SaveAllResults(config.output_folder, 'all')
 
 if __name__ == '__main__':
     amazed()
