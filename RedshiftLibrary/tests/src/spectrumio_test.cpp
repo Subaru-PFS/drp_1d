dae9b140576383a63d935eedeb9815346f060731
guillaume pernot <guillaume.pernot@lam.fr>
2018-06-19 11:48:17 +0200

    fixed spectrumio_test

--- RedshiftLibrary/tests/src/spectrumio_test.cpp
+++ RedshiftLibrary/tests/src/spectrumio_test.cpp
@@ -30,11 +30,11 @@ BOOST_AUTO_TEST_CASE(VVDSReadInvalidFile)
 
     CSpectrum spectrum;
 
-    BOOST_CHECK_NO_THROW(reader.Read( DATA_ROOT_DIR "SpectrumioTestCase/invalidspectrum1.fits",
-				      spectrum ));
-    BOOST_CHECK( spectrum.GetSampleCount() == 0 );
-    BOOST_CHECK_CLOSE_FRACTION( 0.0, spectrum.GetLambdaRange().GetBegin(), 0.01 );
-    BOOST_CHECK_CLOSE_FRACTION( 0.0, spectrum.GetResolution(), 0.01  );
+    BOOST_CHECK_THROW(reader.Read( DATA_ROOT_DIR "SpectrumioTestCase/invalidspectrum1.fits",
+				   spectrum ), std::string);
+    //BOOST_CHECK( spectrum.GetSampleCount() == 0 );
+    //BOOST_CHECK_CLOSE_FRACTION( 0.0, spectrum.GetLambdaRange().GetBegin(), 0.01 );
+    //BOOST_CHECK_CLOSE_FRACTION( 0.0, spectrum.GetResolution(), 0.01  );
 
 }
 
