1ae3a721748c019383ee2356eaa1959812ad05fb
Alain Schmitt <alain.schmitt@lam.fr>
2018-06-26 14:49:41 +0200

    pdf: gaussfit for candidates

--- RedshiftLibrary/RedshiftLibrary/statistics/pdfcandidateszresult.h
+++ RedshiftLibrary/RedshiftLibrary/statistics/pdfcandidateszresult.h
@@ -23,8 +23,18 @@ public:
     Int32 Compute(std::vector<Float64> zc , std::vector<Float64> Pdfz, std::vector<Float64> PdfProbalog);
 
     TFloat64List           		Redshifts;
+    Int32                       optMethod; //0: direct integration, 1:gaussian fit
     TFloat64List           		ValSumProba;
     Float64                     Fullwidth;
+    //opt 1: direct integration
+    //
+    //opt 2: gaussian fit
+    TFloat64List           		GaussAmp;
+    TFloat64List           		GaussAmpErr;
+    TFloat64List           		GaussSigma;
+    TFloat64List           		GaussSigmaErr;
+    //TFloat64List           		GaussSkewness;
+
 };
 
 
