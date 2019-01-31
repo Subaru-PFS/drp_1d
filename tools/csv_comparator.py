3afce71c96632b2701196a1a82f3ab47d574f960
Florian Fauchier <florian.fauchier@lam.fr>
2019-01-31 10:01:32 +0100

    first version of csv_compartor finished

--- tools/csv_comparator.py
+++ tools/csv_comparator.py
@@ -5,152 +5,189 @@ Created on: 28/01/19
 Author: CeSAM
 """
 
-import argparse
+import os
 import csv
+import argparse
 
 # GLOBAL VARIABLES FOR COMPARISION PRECISION
 FLOAT_PRECISION = 1e-12
-REDSHIFT_PRECISION = 1e-12
-MERIT_PRECISION = 1e-12
-SIGME_PRECISION = 1e-12
-INTGPROBA_PRECISION = 1e-12
-GAUSSAMP_PRECISION = 1e-12
-GAUSSAMPERR_PRECISION = 1e-12
-GAUSSSIGMA_PRECISION = 1e-12
-GAUSSSIGMAER_PRECISION = 1e-12
+# MERIT_PRECISION = 1e-12
+# SIGMA_PRECISION = 1e-12
+# REDSHIFT_PRECISION = 1e-12
+# GAUSSAMP_PRECISION = 1e-12
+# INTGPROBA_PRECISION = 1e-12
+# GAUSSSIGMA_PRECISION = 1e-12
+# GAUSSAMPERR_PRECISION = 1e-12
+# GAUSSSIGMAER_PRECISION = 1e-12
 
-# ------------------------------------------------------------------------------
+
+strCmp = str.__eq__
+trueCmp = lambda x, y: True
+
+def float_cmp(precision):
+    return lambda x,y: abs(float(x)-float(y)) < precision
+
+
+def int_cmp():
+    return lambda x,y: int(x) == int(y)
+
+
+def read_spectrumlist(spectrumListFile):
+    spectrumList = []
+    for e in open(spectrumListFile, 'r'):
+        spectrumList.append(e.split(' ')[2].strip())
+
+    return spectrumList
 
 
 class ResultsComparator():
-    def factory(type):
-        if type == "redshift.csv":
-            return Redshift():
-        elif type == "candidatesresult.csv":
-            return Candidate():
-        elif type == "redshiftresult.csv":
-            return RedshiftResult():
-        elif type == "classificationresult.csv":
-            return ClassificationResult():
-
-# ------------------------------------------------------------------------------
 
 
-class Redshift(ResultsComparator):
-    def __init__():
-        self.referencePath = "/home/ffauchier/amazed/"
-
-        self.dict = {
-        "#Spectrum": lambda x,y : x == y,
-        "ProcessingID": lambda x,y : True,
-        "Redshift": lambda x,y : abs(float(x)-float(y)) < REDSHIFT_PRECISION,
-        "Merit": lambda x,y : abs(float(x)-float(y)) < MERIT_PRECISION,
-        "Template": lambda x,y : x == y,
-        "Method": lambda x,y : x == y,
-        "Deltaz": lambda x,y : int(x) == int(y),
-        "Reliability": lambda x,y : x == y,
-        "snrHa": lambda x,y : float(x) == float(y),
-        "lfHa": lambda x,y : float(x) == float(y),
-        "snrOII": lambda x,y : float(x) == float(y),
-        "lfOII": lambda x,y : float(x) == float(y),
-        # "Type": lambda x,y : x == y
-        "Type": True
-        }
-
-# ------------------------------------------------------------------------------
+    def compare(self, path1, path2):
+        pass
 
 
-class RedshiftResult(ResultsComparator):
-    def __init__():
-        self.referencePath = "/home/ffauchier/amazed/"
-
-        self.dict = {
-        "#Redshift": lambda x,y : abs(float(x)-float(y)) < REDSHIFT_PRECISION,
-        "Merit": lambda x,y : abs(float(x)-float(y)) < MERIT_PRECISION,
-        "TemplateRatio": lambda x,y : x == y,
-        "TemplateContinuum": lambda x,y : x == y,
-        "method": lambda x,y : x == y,
-        "sigma": lambda x,y : abs(float(x)-float(y)) < SIGME_PRECISION
-        }
-
-# ------------------------------------------------------------------------------
+class OutputDirComparator(ResultsComparator):
 
 
-class Candidate(ResultsComparator):
-    def __init__():
-        self.referencePath = "/home/ffauchier/amazed/"
+    def compare(self, path1, path2, spectrumListFile):
 
-        self.dict = {
-        "#rank": lambda x,y : int(x)-int(y),
-        "redshift": lambda x,y : abs(float(x)-float(y)) < REDSHIFT_PRECISION,
-        "intgProba": lambda x,y : abs(float(x)-float(y)) < INTGPROBA_PRECISION,
-        "gaussAmp": lambda x,y : abs(float(x)-float(y)) < GAUSSAMP_PRECISION,
-        "gaussAmpErr": lambda x,y : abs(float(x)-float(y)) < GAUSSAMPERR_PRECISION,
-        "gaussSigma": lambda x,y : abs(float(x)-float(y)) < GAUSSSIGMA_PRECISION,
-        "gaussSigmaErr": lambda x,y : abs(float(x)-float(y)) < GAUSSSIGMAER_PRECISION
-        }
-
-# ------------------------------------------------------------------------------
+        spectrumList = read_spectrumlist(spectrumListFile)
+        result = Redshift().compare(path1, path2)
+        for spectrum in spectrumList:
+            for method in [CandidateResult,
+                           RedshiftResult,
+                           ClassificationResult]:
+                r = method().compare(os.path.join(path1, spectrum),
+                                     os.path.join(path2, spectrum))
+                if r:
+                    result.append(r)
+        return result
 
 
-class ClassificationResult(ResultsComparator):
-    def __init__():
-        self.referencePath = "/home/ffauchier/amazed/"
+class CSVComparator():
+    tests = {}
+    filename = ""
+    key = None
+    headerMark = None
 
-        self.dict = {
-        "#Type": lambda x,y : x == y,
-        "Merit": lambda x,y : abs(float(x)-float(y)) < MERIT_PRECISION,
-        "EvidenceG": lambda x,y : abs(float(x)-float(y)) < FLOAT_PRECISION,
-        "EvidenceS": lambda x,y : abs(float(x)-float(y)) < MERIT_PRECISION,
-        "EvidenceQ": lambda x,y : abs(float(x)-float(y)) < MERIT_PRECISION
-        }
+    def _read_csv(self, args):
+        csvFile = open(args)
+        for l in csvFile:
+            if l.startswith(self.headerMark):
+                header = l.split()
+                break
+        reader = csv.reader(csvFile, delimiter='\t')
+        dict_list = {}
 
-# ------------------------------------------------------------------------------
+        for line in reader:
+            if not line:
+                #strip empty lines
+                continue
+            res = dict(zip(header, line))
+            dict_list.update({res[self.key]: res})
+        return dict_list
+
+
+    def compare(self, path1, path2):
+
+        csv1 = self._read_csv(os.path.join(path1, self.filename))
+        csv2 = self._read_csv(os.path.join(path2, self.filename))
+        result = []
+
+
+        for spectrum in csv1.keys():
+            for k in csv1[spectrum].keys():
+                try:
+                    if not self.tests[k](csv1[spectrum][k], csv2[spectrum][k]):
+                        result.append("{} : [{}] != [{}]".format(k, csv1[spectrum][k], csv2[spectrum][k]))
+                except Exception as e :
+                    print("error on key {}".format(k))
+                    raise
+        return result
+
+
+class Redshift(CSVComparator):
+    filename = "redshift.csv"
+    key = '#Spectrum'
+    headerMark = '#Spectrum'
+    tests = {
+    "#Spectrum": strCmp,
+    "ProcessingID": strCmp,
+    "Redshift": float_cmp(FLOAT_PRECISION),
+    "Merit": float_cmp(FLOAT_PRECISION),
+    "Template": strCmp,
+    "Method": strCmp,
+    "Deltaz": float_cmp(FLOAT_PRECISION),
+    "Reliability": strCmp,
+    "snrHa": float_cmp(FLOAT_PRECISION),
+    "lfHa": float_cmp(FLOAT_PRECISION),
+    "snrOII": float_cmp(FLOAT_PRECISION),
+    "lfOII": float_cmp(FLOAT_PRECISION),
+    "Type": strCmp
+    }
+
+
+class RedshiftResult(CSVComparator):
+    filename = "redshiftresult.csv"
+    key = '#Redshifts'
+    headerMark = '#Redshifts'
+    tests = {
+    "#Redshifts": float_cmp(FLOAT_PRECISION),
+    "Merit": float_cmp(FLOAT_PRECISION),
+    "TemplateRatio": strCmp,
+    "TemplateContinuum": strCmp,
+    "method": strCmp,
+    "sigma": float_cmp(FLOAT_PRECISION)
+    }
+
+
+class CandidateResult(CSVComparator):
+    filename = "candidatesresult.csv"
+    key = '#rank'
+    headerMark = '#rank'
+    tests = {
+    "#rank": int_cmp(),
+    "redshift": float_cmp(FLOAT_PRECISION),
+    "intgProba": float_cmp(FLOAT_PRECISION),
+    "gaussAmp": float_cmp(FLOAT_PRECISION),
+    "gaussAmpErr": float_cmp(FLOAT_PRECISION),
+    "gaussSigma": float_cmp(FLOAT_PRECISION),
+    "gaussSigmaErr": float_cmp(FLOAT_PRECISION)
+    }
+
+
+class ClassificationResult(CSVComparator):
+    filename = "classificationresult.csv"
+    key = "#Type"
+    headerMark = "#Type"
+    tests = {
+    "#Type": strCmp,
+    "Merit": float_cmp(FLOAT_PRECISION),
+    "EvidenceG": float_cmp(FLOAT_PRECISION),
+    "EvidenceS": float_cmp(FLOAT_PRECISION),
+    "EvidenceQ": trueCmp
+    }
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('output_dir',
+    parser.add_argument('referenceDir',
                         type=str,
-                        help='Path to the "output" directory.')
-    parser.add_argument('spectrum_list',
+                        help='Path to the output reference directory.')
+    parser.add_argument('outputDir',
+                        type=str,
+                        help='Path to the output tests directory.')
+    parser.add_argument('spectrumListFile',
                         type=str,
                         help='Path to spectrum.list file.')
     args = parser.parse_args()
-    return run(args)
 
-# ------------------------------------------------------------------------------
+    r = OutputDirComparator().compare(args.referenceDir,
+                                      args.outputDir,
+                                      args.spectrumListFile)
+    if r: print(r)
 
-
-def read_csv(args):
-    with open(args) as csv_file:
-        reader = csv.reader(csv_file, delimiter='\t')
-        dict_list = {}
-        header = next(reader)
-        for line in reader:
-            res = dict(zip(header, line))
-            dict_list.update({res['#Spectrum']: res})
-    return dict_list
-
-# ------------------------------------------------------------------------------
-
-
-def run(args):
-    # TODO: definir le type en fonction du fichier donné
-    # TODO: construire les path avec les args OUTPUT
-
-    readerRef = read_csv(args.file1) # ref
-    reader2 = read_csv(args.file2) # res
-
-    obj = Redshift.factory(type)
-    dictRes = obj.dict
-    dict_cmp = obj.referencePath
-
-    for spectrum in readerRef.keys():
-        for k in readerRef[spectrum].keys():
-            assert cmpDict[k](readerRef[spectrum][k],dictRes[spectrum][k])
-
-# ------------------------------------------------------------------------------
 
 if __name__ == '__main__':
     main()
