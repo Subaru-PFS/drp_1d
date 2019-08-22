df0e7bc8aafcd18d7d7c29221925ca4f773dbe36
Chabaud <pierre-yves.chabaud@lam.fr>
2019-08-22 15:40:52 +0200

    Fix unused output_compare test and add new version unit test

---
+++ tests/test_version.py
@@ -0,0 +1,12 @@
+"""
+File: test_version.py
+
+Created on: 22/08/19
+Author: CeSAM
+"""
+
+from pylibamazed.redshift import get_version
+
+def test_version():
+    assert get_version()
+    print(get_version())
