4e2f5eef23c79c5cb5039063315c78051822e5fe
Didier Vibert <didier.vibert@lam.fr>
2019-11-07 16:43:10 +0100

    nshift = ntpl - nspc + 1

--- RedshiftLibrary/src/lib/operator/chisquareloglambda.cpp
+++ RedshiftLibrary/src/lib/operator/chisquareloglambda.cpp
@@ -914,8 +914,7 @@ Int32 COperatorChiSquareLogLambda::FitRangez(Float64 *spectrumRebinedLambda,
     }
 
     //
-    Int32 nshifts = nTpl - nSpc; //+ 1;
-    // Int32 nshifts = nTpl*2.0;
+    Int32 nshifts = nTpl - nSpc + 1;
     m_nPaddedSamples = (Int32)nTpl * 2.0;
     /*
     //next power of two
@@ -941,13 +940,12 @@ Int32 COperatorChiSquareLogLambda::FitRangez(Float64 *spectrumRebinedLambda,
     InitFFT(m_nPaddedSamples);
 
     // prepare z array
-    Int32 zoff = 1;
     std::vector<Float64> z_vect(nshifts, 0.0);
     Int32 z_vect_size = z_vect.size();
     for (Int32 t = 0; t < z_vect_size; t++)
     {
-        z_vect[t] = (spectrumRebinedLambda[0] - tplRebinedLambda[t + zoff]) /
-                    tplRebinedLambda[t + zoff];
+        z_vect[t] = (spectrumRebinedLambda[0] - tplRebinedLambda[t]) /
+                    tplRebinedLambda[t];
     }
 
     // Estimate DtD
