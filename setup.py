f24562b0335ea28c8438caadffee81a8230c6866
Chabaud <pierre-yves.chabaud@lam.fr>
2020-04-02 15:34:43 +0200

    Up to dev5710

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ class CustomBuild(build):
 
 setup(
     name="pylibamazed",
-    version="0.9.0.dev5618",
+    version="0.9.0.dev5710",
     author="LAM - Laboratoire d'Astrophysique de Marseille",
     author_email="amazed-support@lam.fr",
     description=("AMAZED python library."),
