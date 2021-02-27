4597d0e620d42696cada4b0e2d8ef60ce37f5a40
Chabaud <pierre-yves.chabaud@lam.fr>
2021-02-27 16:02:27 +0100

    Set version to 0.20

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ class CustomBuild(build):
 
 setup(
     name="pylibamazed",
-    version="0.18.0",
+    version="0.20",
     author="LAM - Laboratoire d'Astrophysique de Marseille",
     author_email="amazed-support@lam.fr",
     description=("AMAZED python library."),
