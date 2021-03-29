3593f4b797ade5e81d0eac89728ac50710f8a8a3
MARGUERITE Edouard <edouard.marguerite@lam.fr>
2021-03-29 11:00:57 +0200

    New version number strategy

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup
 from distutils.command.build import build
 from swig_ext import swig_ext
 
+root_dir = os.path.dirname(__file__)
+version_file = os.path.join(root_dir, 'VERSION')
+version = open(version_file).read().strip()
+
 class CustomBuild(build):
     def run(self):
         self.run_command('build_ext')
@@ -10,7 +14,13 @@ class CustomBuild(build):
 
 setup(
     name="pylibamazed",
-    version="0.20",
+    version_config={
+        "count_commits_from_version_file": True,
+        "template": version,
+        "dev_template": version+".dev{sha}",
+        "dirty_template": version+".dev{sha}.dirty",
+        "version_file": version_file
+    },
     author="LAM - Laboratoire d'Astrophysique de Marseille",
     author_email="amazed-support@lam.fr",
     description=("AMAZED python library."),
@@ -21,8 +31,8 @@ setup(
     include_package_data=True,
     long_description=open(os.path.join(os.path.dirname(__file__),
                                        'README.md')).read(),
-    setup_requires=['numpy>=1.16.0', 'astropy>=3.1.1', 'cython>=0.17.0', 'pandas>=1.0.0', 'h5py>=2.9'],
-    install_requires=['numpy>=1.16.0', 'astropy>=3.1.1', 'cython>=0.17.0', 'pandas>=1.0.0', 'h5py>=2.9'],
+    setup_requires=['setuptools-git-versioning>=1.2.0', 'numpy>=1.16.0', 'astropy>=3.1.1', 'cython>=0.17.0', 'pandas>=1.0.0', 'h5py>=2.9'],
+    install_requires=['setuptools-git-versioning>=1.2.0', 'numpy>=1.16.0', 'astropy>=3.1.1', 'cython>=0.17.0', 'pandas>=1.0.0', 'h5py>=2.9'],
     tests_require=['pytest-runner', 'pytest', ],
     py_modules=['pylibamazed/redshift'],
     classifiers=[
