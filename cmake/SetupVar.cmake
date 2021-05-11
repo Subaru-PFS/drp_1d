024206fce222c065361c30e6406f23e28c7d004c
MARGUERITE Edouard <edouard.marguerite@lam.fr>
2021-05-11 14:30:50 +0200

    Add CODE_COVERAGE option and coverage rules in CMake

--- cmake/SetupVar.cmake
+++ cmake/SetupVar.cmake
@@ -2,11 +2,13 @@
 
 
 # Force build type to release if not defined
-if(NOT CMAKE_BUILD_TYPE)
+if(NOT CMAKE_BUILD_TYPE AND NOT CODE_COVERAGE)
   set(CMAKE_BUILD_TYPE Release)
+elseif(CODE_COVERAGE)
+  set(CMAKE_BUILD_TYPE Debug)
 endif()
 
-message (STATUS "CMAKE_BUILD_TYPE =" ${CMAKE_BUILD_TYPE} )
+message( STATUS "CMAKE_BUILD_TYPE = " ${CMAKE_BUILD_TYPE} )
 
 #--------------------------------------------------------
 # Compiler options
@@ -14,14 +16,14 @@ message (STATUS "CMAKE_BUILD_TYPE =" ${CMAKE_BUILD_TYPE} )
 
 set(CMAKE_CXX_FLAGS_RELEASE "-O3")
 
-set(CMAKE_CXX_FLAGS_DEBUG " -g -DDEBUG_BUILD")
+set(CMAKE_CXX_FLAGS_DEBUG "-g -DDEBUG_BUILD")
 
 add_definitions("-std=c++11 -Wno-deprecated -Werror=return-type")
 
-if(${CMAKE_BUILD_TYPE}  STREQUAL "Release")
-  message( STATUS "CMAKE_CXX_FLAGS  = " ${CMAKE_CXX_FLAGS_RELEASE} )
+if(${CMAKE_BUILD_TYPE}  STREQUAL "Release" AND NOT CODE_COVERAGE)
+  message( STATUS "CMAKE_CXX_FLAGS = " ${CMAKE_CXX_FLAGS_RELEASE} )
   set(EXT "")
 else()
-  message( STATUS "CMAKE_CXX_FLAGS  = " ${CMAKE_CXX_FLAGS_DEBUG} )
+  message( STATUS "CMAKE_CXX_FLAGS = " ${CMAKE_CXX_FLAGS_DEBUG} )
   set(EXT "-debug")
 endif()
