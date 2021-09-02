d81ed8110a76fb9783f7205c58c63427b1c3077d
Ali Allaoui <ali.allaoui@lam.fr>
2021-09-02 17:19:38 +0200

    Handle galaxysolve=no in classification and reliability solvers

--- RedshiftLibrary/src/lib/method/reliabilitysolve.cpp
+++ RedshiftLibrary/src/lib/method/reliabilitysolve.cpp
@@ -56,61 +56,56 @@ std::shared_ptr<CSolveResult> CReliabilitySolve::compute(std::shared_ptr<const C
 
   
   std::shared_ptr<const CPdfSolveResult> galaxyResult=std::shared_ptr<const CPdfSolveResult>(nullptr); //std::make_shared<const CPdfSolveResult>();
-
-  galaxyResult = std::dynamic_pointer_cast<const CPdfSolveResult>(resultStore->GetSolveResult("galaxy"));
-
-  /*
   std::shared_ptr<const CPdfSolveResult> starResult=std::shared_ptr<const CPdfSolveResult>(nullptr);//std::make_shared<const CPdfSolveResult>();
   std::shared_ptr<const CPdfSolveResult> qsoResult=std::shared_ptr<const CPdfSolveResult>(nullptr);
 
+  if(inputContext->GetParameterStore()->Get<std::string>("enablegalaxysolve") == "yes")    
+    galaxyResult = std::dynamic_pointer_cast<const CPdfSolveResult>(resultStore->GetSolveResult("galaxy"));
   if(inputContext->GetParameterStore()->Get<std::string>("enablestellarsolve") == "yes")
-    starResult =  std::dynamic_pointer_cast<const CPdfSolveResult>(resultStore->GetGlobalResult("star.result").lock());
+    starResult =  std::dynamic_pointer_cast<const CPdfSolveResult>(resultStore->GetSolveResult("star"));
   if(inputContext->GetParameterStore()->Get<std::string>("enableqsosolve") == "yes")
-    qsoResult =  std::dynamic_pointer_cast<const CPdfSolveResult>(resultStore->GetGlobalResult("qso.result").lock());
+    qsoResult =  std::dynamic_pointer_cast<const CPdfSolveResult>(resultStore->GetSolveResult("qso"));
 
-
-Float64 qsoLogEvidence = -INFINITY ;
-    Float64 stellarLogEvidence = -INFINITY;
-
-
-    if(starResult){
-    stellarLogEvidence = starResult->getEvidence();
-        Log.LogInfo( "Found stellar LogEvidence: %e", stellarLogEvidence);
-        if(stellarLogEvidence > MaxLogEvidence)
-        {
-          merit = starResult->getMerit();
-        }
-    }
-
-    if(qsoResult){
-        qsoLogEvidence = qsoResult->getEvidence();
-        Log.LogInfo( "Found qso LogEvidence: %e", qsoLogEvidence);
-        if(qsoLogEvidence>MaxLogEvidence)
-        {
-          merit = qsoResult->getMerit();
-        }
-    }
-  */
-    std::shared_ptr<CReliabilityResult> reliabResult = std::make_shared<CReliabilityResult>();
+  Float64 galaxyLogEvidence = -INFINITY;
+  Float64 qsoLogEvidence = -INFINITY ;
+  Float64 stellarLogEvidence = -INFINITY;
+  
+  std::shared_ptr<CReliabilityResult> reliabResult = std::make_shared<CReliabilityResult>();
   Float64 MaxLogEvidence = -INFINITY;    
-    Float64 galaxyLogEvidence = -INFINITY;
+
     
-    Float64 merit = 0;
-    if(galaxyResult){
-        galaxyLogEvidence = galaxyResult->getEvidence();
-        Log.LogInfo( "Found galaxy LogEvidence: %e", galaxyLogEvidence);
-        if (galaxyLogEvidence > MaxLogEvidence){
-          merit = galaxyResult->getMerit();
-        }
+  Float64 merit = 0;
+  if(galaxyResult){
+    galaxyLogEvidence = galaxyResult->getEvidence();
+    Log.LogInfo( "Found galaxy LogEvidence: %e", galaxyLogEvidence);
+    if (galaxyLogEvidence > MaxLogEvidence){
+      merit = galaxyResult->getMerit();
     }
+  }
+  if(starResult){
+    stellarLogEvidence = starResult->getEvidence();
+    Log.LogInfo( "Found stellar LogEvidence: %e", stellarLogEvidence);
+    if(stellarLogEvidence > MaxLogEvidence)
+      {
+	merit = starResult->getMerit();
+      }
+  }
+
+  if(qsoResult){
+    qsoLogEvidence = qsoResult->getEvidence();
+    Log.LogInfo( "Found qso LogEvidence: %e", qsoLogEvidence);
+    if(qsoLogEvidence>MaxLogEvidence)
+      {
+	merit = qsoResult->getMerit();
+      }
+  }
+
+  if (std::isnan(merit)) reliabResult->m_ReliabilityLabel="C6";                                           else {
+    int reliability = 6 - floor(merit*6);
+    if (reliability == 0) reliability = 1;
+    std::ostringstream os;                                                                                  os << "C" << reliability;
+    reliabResult->m_ReliabilityLabel=os.str();                                                                 }                                     
 
 
-    if (std::isnan(merit)) reliabResult->m_ReliabilityLabel="C6";                                           else {
-      int reliability = 6 - floor(merit*6);
-      if (reliability == 0) reliability = 1;
-      std::ostringstream os;                                                                                  os << "C" << reliability;
-      reliabResult->m_ReliabilityLabel=os.str();                                                                 }                                     
-
-
-    return reliabResult;
+  return reliabResult;
 }
