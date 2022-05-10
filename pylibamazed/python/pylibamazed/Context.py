4b0f87e93860db8e991992ad779935839974b2d3
Ali Allaoui <ali.allaoui@lam.fr>
2022-05-10 11:12:38 +0200

    SubType method

--- pylibamazed/python/pylibamazed/Context.py
+++ pylibamazed/python/pylibamazed/Context.py
@@ -42,6 +42,7 @@ from pylibamazed.CalibrationLibrary import CalibrationLibrary
 from pylibamazed.ResultStoreOutput import ResultStoreOutput
 from pylibamazed.Reliability import Reliability
 from pylibamazed.Parameters import Parameters
+from pylibamazed.SubType import SubType
 from pylibamazed.redshift import (CProcessFlowContext,
                                   CLineModelSolve,
                                   CTemplateFittingSolve,
@@ -120,6 +121,7 @@ class Context:
 
             enable_classification = False
             reliabilities = dict()
+            sub_types = dict()
             for object_type in self.parameters.get_objects():
                 method = self.parameters.get_solve_method(object_type)
                 if method:
@@ -140,6 +142,12 @@ class Context:
                 if self.parameters.reliability_enabled(object_type) and object_type in self.calibration_library.reliability_models:
                     rel = Reliability(object_type, self.parameters,self.calibration_library)
                     reliabilities[object_type] = rel.Compute(self.process_flow_context)
+            	if method == "LineModelSolve" and self.parameters[object_type]["LineModelSolve"]["linemodel"]["rigidity"] == "tplshape":
+                	sub_classif = SubType(object_type,
+                                      self.parameters,
+                                      self.calibration_library)
+                	sub_types[object_type] = sub_classif.Compute(self.process_flow_context)
+            
 
             if enable_classification:
                 self.run_method("classification", "ClassificationSolve")
@@ -150,6 +158,8 @@ class Context:
             for object_type in reliabilities.keys():
                 rso.object_results[object_type]['reliability'] = dict()
                 rso.object_results[object_type]['reliability']['Reliability'] = reliabilities[object_type]
+            for object_type in sub_types.keys():
+                rso.object_results[object_type]['model_parameters']['SubType'] = sub_types[object_type]
 
             return rso
         except GlobalException as e:
