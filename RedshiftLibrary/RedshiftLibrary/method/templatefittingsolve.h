64a4650932fb5db0d83200077ae504e5f6530207
gdaste <gaelle.daste@lam.fr>
2023-01-26 11:21:14 +0100

    remove saveIntermediateResults option

--- RedshiftLibrary/RedshiftLibrary/method/templatefittingsolve.h
+++ RedshiftLibrary/RedshiftLibrary/method/templatefittingsolve.h
@@ -96,7 +96,6 @@ private:
   std::string m_opt_pdfcombination;
   Float64 m_redshiftSeparation;
   Int64 m_opt_maxCandidate;
-  bool m_opt_enableSaveIntermediateTemplateFittingResults = false;
 };
 
 } // namespace NSEpic
