e4cbd5dff33fb7e63bab49f8e6a12cb96c63f6d1
fdufresne <fanny.dufresne@lam.fr>
2023-10-31 17:55:40 +0100

    Adds new json schema -> jsonschema-v2

--- pylibamazed/python/pylibamazed/r_specifications.py
+++ pylibamazed/python/pylibamazed/r_specifications.py
@@ -42,5 +42,5 @@ module_root_dir = os.path.split(__file__)[0]
 
 rspecifications = os.path.join(module_root_dir, "resources", "results_specifications.csv")
 
-jsonSchemaFilename = os.path.join(module_root_dir, "resources", "general.json")
-jsonSchemaPath = f"file:///{os.path.join(module_root_dir, 'resources')}/"
+jsonSchemaFilename = os.path.join(module_root_dir, "resources", "jsonschema-v1", "general.json")
+jsonSchemaPath = f"file:///{os.path.join(module_root_dir, 'resources', 'jsonschema-v1')}/"
