1c2936cd9bfaea99405e88d1d703c9d115d23b77
fdufresne <fanny.dufresne@lam.fr>
2024-09-20 16:40:49 +0200

    run pre commit on all files

--- RedshiftLibrary/RedshiftLibrary/linemodel/powerlawstore.h
+++ RedshiftLibrary/RedshiftLibrary/linemodel/powerlawstore.h
@@ -51,15 +51,12 @@ class CPowerLawStore : public CContinuumFitStore {
 public:
   using CContinuumFitStore::CContinuumFitStore;
   Int32 getContinuumCount() const override { return 1; };
-  void Add(Float64 ismEbmvCoeff, Int32 igmMeiksinIdx,
-                         Float64 redshift,
-                         Float64 chi2,
-                         TPowerLawCoefsPair powerlawCoefs,
-                         Float64 snr);
+  void Add(Float64 ismEbmvCoeff, Int32 igmMeiksinIdx, Float64 redshift,
+           Float64 chi2, TPowerLawCoefsPair powerlawCoefs, Float64 snr);
 
 private:
-  Float64 getFracAmplitudeSigma(CContinuumModelSolution const &continuum) const override;
-
+  Float64 getFracAmplitudeSigma(
+      CContinuumModelSolution const &continuum) const override;
 };
 
 } // namespace NSEpic
