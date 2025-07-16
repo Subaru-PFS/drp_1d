555fcb1829e27e2a204ecc0cf3301149c263675f
Didier Vibert <didier.vibert@lam.fr>
2025-07-16 18:59:52 +0200

    move spectrum_id in read arguements

--- pylibamazed/python/pylibamazed/AbstractExternalStorage.py
+++ pylibamazed/python/pylibamazed/AbstractExternalStorage.py
@@ -68,41 +68,34 @@ class AbstractExternalStorage:
     constant `READER_CLASSES` in the same module as this class.
     """
 
-    def __init__(self, config, spectrum_id):
+    def __init__(self, config):
         if config.reader not in READER_CLASSES:
             raise Exception(f"Reader class must be one of the following: {READER_CLASSES}")
         self.config = config
-        self.spectrum_id = spectrum_id
+        self._spectrum_id = None
         self.spectrum_infos = dict()
         self.global_infos = dict()
 
     #  to be used as context manager
     def __enter__(self):
-        obs_id, kwargs = self._read_param
-        self.resource = self.read(obs_id, **kwargs)
+        spectrum_id, obs_id = self._call_params
+        del self._call_params
+        self.resource = self.read(spectrum_id, obs_id)
         return self.resource
 
-    def __call__(self, obs_id="", **kwargs):
+    def __call__(self, spectrum_id, obs_id=""):
         # store read parameters
-        self._read_param = (obs_id, kwargs)
+        self._call_params = (spectrum_id, obs_id)
+
         return self
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
-        self._read_param = None
         self.close(self.resource)
         self.resource = None
         return False
 
     @doc_method
-    def set_spectrum_id(self, spectrum_id):
-        self.spectrum_id = spectrum_id
-
-    @doc_method
-    def read(
-        self,
-        obs_id: str = "",
-        **kwargs,
-    ):
+    def read(self, spectrum_id, obs_id: str = ""):
         """
         Read a spectrum file and return its data.
 
@@ -177,22 +170,20 @@ class AbstractExternalStorage:
         spectrum = h5py.File(filepath, "r")
         return spectrum
 
-    def _get_spectrum_path(self, obs_id=""):
+    def _get_spectrum_path(self, spectrum_id, obs_id=""):
         if self.config.spectrum_path_col:
             if obs_id:
                 s_filename = obs_id
             else:
-                s_filename = self.spectrum_id.Path
+                s_filename = spectrum_id.Path
         elif obs_id:
-            s_filename = s_filename = (
+            s_filename = (
                 self.config.spectrum_prefix
-                + self.spectrum_id.ProcessingID
+                + spectrum_id.ProcessingID
                 + "_"
                 + obs_id
                 + self.config.spectrum_suffix
             )
         else:
-            s_filename = (
-                self.config.spectrum_prefix + self.spectrum_id.ProcessingID + self.config.spectrum_suffix
-            )
+            s_filename = self.config.spectrum_prefix + spectrum_id.ProcessingID + self.config.spectrum_suffix
         return os.path.join(self.config.spectrum_dir, s_filename)
