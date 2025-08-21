fix mean proba computation

pylibamazed/python/pylibamazed/DeepLearningSolve.py
@@ -101,10 +101,10 @@ class DeepLearningSolve(AbstractReliabilitySolver):
         classes = model_parameters["classes"]
         results = list()
         for model in models:
-            results.append(model.predict(np.exp(pdfval[None, :, None])))
+            results.append(model.predict(np.exp(pdfval[None, :, None])[0]))
         probas = np.mean(np.array(results), axis=0)
         for i in range(1, len(classes)):
-            ret[classes[i]] = probas[0, i]
+            ret[classes[i]] = probas[i]
         return ret
 
 