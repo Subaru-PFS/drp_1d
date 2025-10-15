71430439371b97459c5efaf89f8bfa865202f5db
fdufresne <fanny.dufresne@lam.fr>
2025-10-15 16:53:25 +0200

    Remove first pass results from TemplateCombination

--- RedshiftLibrary/RedshiftLibrary/processflow/resultstore.h
+++ RedshiftLibrary/RedshiftLibrary/processflow/resultstore.h
@@ -147,7 +147,8 @@ public:
   GetTplCombinationResult(const std::string &spectrumModel,
                           const std::string &stage, const std::string &method,
                           const std::string &name, const std::string &dataset,
-                          const int &rank) const;
+                          const int &rank,
+                          bool firstpassCorrespondingResult = false) const;
   std::shared_ptr<const TExtremaResult>
   GetExtremaResult(const std::string &spectrumModel, const std::string &stage,
                    const std::string &method, const std::string &name,
